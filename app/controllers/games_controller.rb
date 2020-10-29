17a3867e6059a4b07fb80487d2db15142486a510
chance <chancesmith97@hotmail.com>
2020-10-29 15:47:59 -0600

    added show and index page for Genres

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,27 @@
 class GamesController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def new 
+        @game = Game.new
+    end
+
+    def create 
+        @game = current_user.games.build(game_params)
+        binding.pry
+        if @game.save 
+            redirect_to games_path 
+        else
+            render :new 
+        end
+
+    end
+
+
+    private 
+
+    def game_params
+         params.require(:game).permit(:title, :description)
+    end
+
+
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,16 @@
 class GenresController < ApplicationController
+
+    def index 
+        @genre = Genre.all.includes(:games)
+    end
+
+    def show 
+        @genre = Genre.find_by_id(params[:id])
+    end
+
+    def new 
+    end
+    
+
+
 end
