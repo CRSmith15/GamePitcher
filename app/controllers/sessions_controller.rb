63d306be929daaba237689ba6393501b3695e696
chance <chancesmith97@hotmail.com>
2020-10-28 20:37:45 -0600

    added helper methods + create method for a users session (log in)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,22 @@ class SessionsController < ApplicationController
     def home 
     end
 
+    def new 
+    end
+
+    def create 
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id 
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Wrong login info. Try again."
+            redirect_to '/login'
+        end
+
+    end
+
+
     def destroy 
         session.clear 
         redirect_to '/'
