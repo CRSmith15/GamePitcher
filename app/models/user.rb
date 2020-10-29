994a730422ec00d36d7576fda89f87412f9c1c13
chance <chancesmith97@hotmail.com>
2020-10-28 18:32:02 -0600

    added has secure password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :comments
     has_many :commented_games, through: :comments, source: :game 
     has_many :genres, through: :games
+
+    has_secure_password
 end
