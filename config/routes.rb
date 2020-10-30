be18cb1f94cca4ee7bd69bea9757c3bc34af9353
chance <chancesmith97@hotmail.com>
2020-10-29 22:27:19 -0600

    fixed nested route for a game's comments

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   end
 
   resources :games do 
-    resources :comments, only: [:new, :create, :index]
+    resources :comments
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
