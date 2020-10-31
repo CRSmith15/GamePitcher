c0f9b2f218c5c05dacc32d7910ed08d1df2579c3
chance <chancesmith97@hotmail.com>
2020-10-31 00:18:31 -0600

    fixed pitch_limit

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,7 +13,7 @@ class Game < ApplicationRecord
     todays_pitches = user.games.select do |g|
       g.created_at.try(:to_date) == Date.today
     end
-    if todays_pitches.size > 1
+    if todays_pitches.size > 3
       errors.add(:game_id, "No more than 3 new pitches per day.")
     end
   end
