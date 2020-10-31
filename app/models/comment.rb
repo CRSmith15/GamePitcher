bd4697eaa53635bbe5665a959c82a71ee59ed126
chance <chancesmith97@hotmail.com>
2020-10-31 00:16:37 -0600

    added validation to stop too many games to be created in a day

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :game
   validates :text, presence: true
+
+  validates :game, uniqueness: {scope: :user_id, message: "may only have one comment per user."}
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -4,5 +4,18 @@ class Game < ApplicationRecord
   has_many :comments
   has_many :users, through: :comments
 
-  validates :title, :description, :genre, presence: true
+  validates :title, :description, presence: true
+  validate :pitch_limit, :on => :create
+
+
+  def pitch_limit 
+    #binding.pry
+    todays_pitches = user.games.select do |g|
+      g.created_at.try(:to_date) == Date.today
+    end
+    if todays_pitches.size > 1
+      errors.add(:game_id, "No more than 3 new pitches per day.")
+    end
+  end
+
 end
